159cc7015c9b7d3b269de8ad6f7660e3d4180e03
Brvnaxs <xavierbruna9@gmail.com>
2022-10-19 18:22:34 -0300

    Atualizações

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-document.addEventListener('DOMContentLoaded', event => {
-    event.preventDefault()
-    fetch("https://dummyapi.io/data/v1/post?limit=10")
-    .then(res => res.json())
-    .then(posts => {
-        posts.map( post => {
-            const div = document.createElement('div')
-        })
-    })
-})
+//Próxima etapa.
+
+
+
