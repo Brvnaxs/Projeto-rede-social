aa2e69bc1993dd1908e8d39e5f62c578b2b8a29e
Brvnaxs <xavierbruna9@gmail.com>
2022-11-01 21:31:28 -0300

    Atualizaçoes

--- app.js
+++ app.js
@@ -1,79 +1,5 @@
-//Próxima etapa.
-// const body = document.body
-// const post = document.createElement('div')
-// post.classList.add('post')
-// const secaoUsuario = document.createElement('div')
-// secaoUsuario.classList.add('headerPost')
-// const ftUsuario = document.createElement('img')
-// ftUsuario.classList.add('fotoUsuario')
-// const nomeUsuario = document.createElement('h1')
-// nomeUsuario.classList.add('nomeUsuario')
-// const botaoOpcoes = document.createElement('botao')
-// const botaocontent = document.createTextNode("...")
-// botaoOpcoes.setAttribute(botaocontent)
-// botaoOpcoes.classList.add('botao')
-// secaoUsuario.appendChild(ftUsuario)
-// secaoUsuario.appendChild(nomeUsuario)
-// secaoUsuario.appendChild(botaoOpcoes)
-// //lllllllllllllllllllllllll
-// const ftPost = document.createElement('img')
-// ftPost.setAttribute('href','src/Mae&filho.png')
-// ftPost.classList.add('fotoPost')
-// const botoes = document.createElement('div')
-// botoes.classList.add('botoes')
-// const bt1 = document.createElement('button')
-// bt1.classList.add('botao')
-// const bt2 = document.createElement('button')
-// bt2.classList.add('botao')
-// const bt3 = document.createElement('button')
-// bt3.classList.add('botao')
-// //conteudo
-// const textoPost = document.createElement('p')
-// textoPost.classList.add('textoPost')
-// const areaTexto = document.createElement('div')
-// areaTexto.classList.add('areaTexto')
-// areaTexto.appendChild(nomeUsuario)
-// areaTexto.appendChild(textoPost)
-// botoes.appendChild(bt1)
-// botoes.appendChild(bt2)
-// botoes.appendChild(bt3)
-// post.appendChild(secaoUsuario)
-// post.appendChild(ftPost)
-// post.appendChild(botoes)
-// post.appendChild(areaTexto)
-// const timeline = document.querySelector('#timeline')
-// post.style.backgroundColor = "black"
-// body.appendChild(post)
 
-//novo teste:
 const timeline = document.querySelector('#timeline')
-// const nm = document.createTextNode("@novo.user")
-// //timeline.append('<div class="post">'+'<section class="headerPost">'+'<img class="fotoUsuario" src="src/Pigmeu.jpg" alt="">'+'<h1 class="nomeUsuario">'+nm+'</h1>'+'<button class="botao">...</button>'+'</section>'+'<img class="fotoPost" src="src/Mae&filho.png" alt="">'+'<section class="botoes">'+'<button class="botao"><img src="src/like.png" alt=""></button>'+'<button class="botao"><img src="src/coment.png" alt=""></button>'+'<button class="botao"><img src="src/send.png" alt=""></button>'+'</section>'+'<section class="areaTexto">'+'<p class="nomeUsuario">@Baby_Hipo</p>'+'<p class="textoPost">Me and mommy 💕💕</p>'+'</section>'+'</div>')
-// let testePost = '<div class="post">'+
-// '<section class="headerPost">'+
-// '<img class="fotoUsuario" src="src/Pigmeu.jpg" alt="">'+
-// '<h1 class="nomeUsuario">@Baby_Hipo</h1>'+
-// '<button class="botao">...</button>'+
-// '</section>'+
-// '<img class="fotoPost" src="src/Mae&filho.png" alt="">'+
-// '<section class="botoes">'+
-// '<button class="botao"><img src="src/like.png" alt=""></button>'+
-// '<button class="botao"><img src="src/coment.png" alt=""></button>'+
-// '<button class="botao"><img src="src/send.png" alt=""></button>'+
-// '</section>'+
-// '<section class="areaTexto">'+
-// '<p class="nomeUsuario">@Baby_Hipo</p>'+
-// '<p class="textoPost">Me and mommy 💕💕</p></section>'+
-// '</div>'
-// // let parser = new DOMParser();
-
-// // let doc = new DOMParser().parseFromString(testePost, "text/html");
-
-// let s = $.parseHTML(testePost)
-// let ll = $.parseHTML(testePost)
-// $('#timeline').append(ll)
-// $('#timeline').append(s)
-
 window.addEventListener('DOMContentLoaded', e => {
     e.preventDefault()
 
@@ -83,8 +9,8 @@ window.addEventListener('DOMContentLoaded', e => {
             users.forEach(user => {
                 fetch('https://dummyjson.com/posts/'+user.id)
                 .then(res => res.json())
-                .then((post) =>{
-                        const u = `<div class="post">
+                .then((posts) =>{
+                        const postagem = `<div class="post">
                 <section class="headerPost">
                 <img class="fotoUsuario" src="${user.image}"alt="">
                 <h1 class="nomeUsuario">${user.username}</h1>
@@ -98,10 +24,10 @@ window.addEventListener('DOMContentLoaded', e => {
                 </section>
                 <section class="areaTexto">
                 <p class="nomeUsuario">${user.username}</p>
-                <p class="textoPost">${post.body}</p>
+                <p class="textoPost">${posts.body}</p>
                 </section>
             </div>`
-                timeline.innerHTML += u
+                timeline.innerHTML += postagem
             }); 
                 
 
@@ -109,3 +35,35 @@ window.addEventListener('DOMContentLoaded', e => {
                 
         })
     })
+
+    // for(var i = 1; i < 3; i++){
+    //     fetch('https://dummyjson.com/users/'+i)
+    //     .then(res => res.json())
+    //     .then((user) => {
+    //        var usuario = document.querySelector(`#user${i}`)
+    //         usuario.setAttribute('src', `${user.image}`)
+    //     })
+    // }
+    
+    fetch('https://dummyjson.com/users/1')
+    .then(res => res.json())
+    .then((user) =>{
+        var usuario = document.getElementById('user1')
+        usuario.setAttribute('src', `${user.image}`)
+    });
+
+    fetch('https://dummyjson.com/users/2')
+    .then(res => res.json())
+    .then((user) =>{
+        var usuario = document.getElementById('user2')
+        usuario.setAttribute('src', `${user.image}`)
+    });
+
+    fetch('https://dummyjson.com/users/3')
+    .then(res => res.json())
+    .then((user) =>{
+        var usuario = document.getElementById('user3')
+        usuario.setAttribute('src', `${user.image}`)
+    });
+
+                
